bd0f337b4bf3e962bd11ff3375d3c03b52a889bf
Aiverydev2 <aiverydev@gmail.com>
2024-08-24 00:36:56 +0300

    Update hmdm_init.ru.sql
    Refind you bag in tag

--- install/sql/hmdm_init.ru.sql
+++ install/sql/hmdm_init.ru.sql
@@ -1,4 +1,4 @@
-UPDATE users SET email='_ADMIN_EMAIL_', passwordReset=true, passwordResetToken=md5(random()::text) WHERE id=1;
+#UPDATE users SET email='_ADMIN_EMAIL_', passwordReset=true, passwordResetToken=md5(random()::text) WHERE id=1;
 
 INSERT INTO settings (id, backgroundcolor, textcolor, backgroundimageurl, iconsize, desktopheader, customerid, usedefaultlanguage, language) VALUES (1, '#678ca6', '#ffffff', NULL, 'SMALL', 'NO_HEADER', 1, true, NULL);
 
